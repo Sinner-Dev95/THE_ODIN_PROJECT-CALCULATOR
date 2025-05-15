3877bb487187be9826b376203ffac7ee66571aff
Sinner-Dev95 <ID+username@users.noreply.github.com>
2025-05-15 18:30:16 +0200

    Create three variables num1, num2, operator. Create function operate that includes the four maths operations.

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,9 @@
+function sepa(){
+    console.log('--------------------------------------------');
+};
+
+
+
 //FUNCTION FOR MATHS OPERATION
 
 function sum (num1, num2){
@@ -24,9 +30,28 @@ function divide (num1, num2){
 
 console.log(divide(2, 2));
 
-function sepa(){
-    console.log('--------------------------------------------');
-};
 sepa();
+
+
+//VARIABLES FOR OPERATORS AND NUMBERS
+
+let num1;
+let num2;
+let operator;
+
+
+// FUNCTION FOR OPERATIONS
+function operate(num1, num2, operator){
+    switch(operator){
+        case '+':
+            return sum(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
 
 
