a251edbfc9eaa7d6de0b60d3342a38866dc32758
Sinner-Dev95 <ID+username@users.noreply.github.com>
2025-05-15 18:23:55 +0200

    Create and connected index.html with css and js file. In js file create the four maths functions.

---
+++ js/calculator.js
@@ -0,0 +1,32 @@
+//FUNCTION FOR MATHS OPERATION
+
+function sum (num1, num2){
+    return num1 + num2;
+}
+
+console.log(sum(2, 2));
+
+function subtract (num1, num2){
+    return num1 - num2;
+}
+
+console.log(subtract(2, 2));
+
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiply(2, 2));
+
+function divide (num1, num2){
+    return num1 / num2;
+}
+
+console.log(divide(2, 2));
+
+function sepa(){
+    console.log('--------------------------------------------');
+};
+sepa();
+
+
